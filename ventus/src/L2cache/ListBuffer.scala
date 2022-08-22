24dd5ec2932c1aa17e9918c928bd0163a2138b6c
lxd21 <lxd21@mails.tsinghua.edu.cn>
2022-08-22 13:57:14 +0800

    add license

--- ventus/src/L2cache/ListBuffer.scala
+++ ventus/src/L2cache/ListBuffer.scala
@@ -1,19 +1,13 @@
 /*
- * Copyright 2019 SiFive, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You should have received a copy of LICENSE.Apache2 along with
- * this software. If not, you may obtain a copy at
- *
- *    https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+ * Copyright (c) 2021-2022 International Innovation Center of Tsinghua University, Shanghai
+ * Ventus is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details. */
 
 package L2cache
 
--- ventus/src/axi/AXI4Lite2CTA.scala
+++ ventus/src/axi/AXI4Lite2CTA.scala
@@ -1,8 +1,13 @@
 /*
- * Copyright (c) 2019-2021 Antmicro <www.antmicro.com>
- *
- * SPDX-License-Identifier: Apache-2.0
- */
+ * Copyright (c) 2021-2022 International Innovation Center of Tsinghua University, Shanghai
+ * Ventus is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details. */
 
 package axi
 
