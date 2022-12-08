0e90a5aaa6aec321b7ed9de7d080543763f423c7
yangkex <962284474@qq.com>
2022-12-08 16:56:43 +0800

    update parameter - lane

--- ventus/src/pipeline/parameters.scala
+++ ventus/src/pipeline/parameters.scala
@@ -24,7 +24,7 @@ object parameters{//notice log2Ceil(4) returns 2.that is ,n is the total num, no
   def xLen = 32 // data length 32-bit
   def num_block=num_warp // not bigger than num_warp
   def num_warp_in_a_block=num_warp
-
+  def num_lane=num_thread/2
   def num_icachebuf = 1 //blocking for each warp
   def depth_icachebuf = log2Ceil(num_icachebuf)
   def num_ibuffer=2
