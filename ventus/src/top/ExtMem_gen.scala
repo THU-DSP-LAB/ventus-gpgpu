89b147f73bc111ebbdc6be37978e72c6b7cf99a2
yzx_ubuntu_sh <yangzexia99@163.com>
2022-12-26 19:07:49 +0800

    改了会出问题的语法， operandCollector.scala line467

--- ventus/src/top/ExtMem_gen.scala
+++ ventus/src/top/ExtMem_gen.scala
@@ -12,8 +12,12 @@ package top
 import L1Cache.MyConfig
 import L2cache.{CacheParameters, InclusiveCacheMicroParameters, InclusiveCacheParameters_lite}
 import pipeline.parameters.{l2cache_BlockWords, l2cache_NSets, l2cache_NWays, l2cache_memCycles, l2cache_portFactor, l2cache_writeBytes, num_sm, num_warp}
+import pipeline.parameters.{l2cache_BlockWords, l2cache_NSets, l2cache_NWays, l2cache_memCycles, l2cache_portFactor, l2cache_writeBytes, num_sm, num_warp}
 //import pipeline.vALUv2TestWrapper
 
+//object GPGPU_gen extends App{
+//  (new chisel3.stage.ChiselStage).emitVerilog(new GPGPU_axi_top())
+//}
 object GPGPU_gen extends App{
   val L1param = (new MyConfig).toInstance
   val L2param = InclusiveCacheParameters_lite(CacheParameters(2, l2cache_NSets, l2cache_NWays, blockBytes = (l2cache_BlockWords << 2), beatBytes = (l2cache_BlockWords << 2)), InclusiveCacheMicroParameters(l2cache_writeBytes, l2cache_memCycles, l2cache_portFactor, num_warp, num_sm), false)
