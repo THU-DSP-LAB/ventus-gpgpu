6311287b6eff634606ae401166b7301feed889bd
Mingyuan <cnwhmmy@outlook.com>
2023-01-19 15:09:35 +0800

    modified:   .vscode/c_cpp_properties.json 	modified:   sc-code/pipeline/Makefile 	modified:   sc-code/pipeline/fetch.h 	new file:   sc-code/pipeline/fetch_test.cpp

--- sc-code/pipeline/fetch.h
+++ sc-code/pipeline/fetch.h
@@ -1,9 +1,11 @@
-#ifndef _FETCH_H
-#define _FETCH_H
+
+#ifndef FETCH_H_
+#define FETCH_H_
 
 #include "../parameters.h"
 
-SC_MODULE(fetch){
+SC_MODULE(fetch)
+{
     sc_in_clk clk{"clk"};
     sc_in<bool> rst_n{"rst_n"};
     sc_in<bool> ibuf_full{"ibuf_full"};
@@ -14,17 +16,16 @@ SC_MODULE(fetch){
     sc_port<event_if> fetch_out{"fetch_out"};
 
     sc_signal<sc_uint<ireg_bitsize>> pc;
-    I_TYPE ireg [ireg_size];
+    I_TYPE ireg[ireg_size];
 
     void PROGRAM_COUNTER();
     void INSTRUCTION_REG();
-    SC_CTOR(fetch){
+    SC_CTOR(fetch)
+    {
         SC_METHOD(PROGRAM_COUNTER);
         sensitive << clk.pos() << rst_n.neg();
         SC_THREAD(INSTRUCTION_REG);
     }
 };
-
-
 
 #endif
---
+++ sc-code/pipeline/fetch_test.cpp
@@ -0,0 +1,5 @@
+#include "fetch.h"
+
+int sc_main(int argc, char *argv[]){
+    
+}
