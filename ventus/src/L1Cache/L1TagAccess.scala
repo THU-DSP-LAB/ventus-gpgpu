7db1c30653ee23e2ebf1d1a395b2c13582bf4778
au@109WSLU20 <auchufeng@qq.com>
2023-03-02 19:17:20 +0800

    timeArray and maxIdxTree

--- ventus/src/L1Cache/L1TagAccess.scala
+++ ventus/src/L1Cache/L1TagAccess.scala
@@ -43,8 +43,10 @@ class L1TagAccess(set: Int, way: Int, tagBits: Int)extends Module{
 
     val hit_st1 = Output(Bool())
   })
+  //TagAccess internal parameters
+  val Length_Replace_time_SRAM: Int = 10
 
-//SRAM to store tag
+  //SRAM to store tag
   val tagBodyAccess = Module(new SRAMTemplate(
     UInt(tagBits.W),
     set=set,
@@ -55,6 +57,19 @@ class L1TagAccess(set: Int, way: Int, tagBits: Int)extends Module{
     bypassWrite = false
   ))
   tagBodyAccess.io.r.req <> io.probeRead
+
+  //SRAM for replacement policy
+  //store last_access_time for LRU, or last_fill_time for FIFO
+  val timeAccess = Module(new SRAMTemplate(
+    UInt(Length_Replace_time_SRAM.W),
+    set = set,
+    way = way,
+    shouldReset = false,
+    holdRead = true,
+    singlePort = false,
+    bypassWrite = false
+  ))
+  timeAccess.io.r.req <> io.allocateWrite
 
   val way_valid = RegInit(VecInit(Seq.fill(set)(VecInit(Seq.fill(way)(0.U(1.W))))))
   //val way_valid = Mem(set, UInt(way.W))
@@ -68,8 +83,8 @@ class L1TagAccess(set: Int, way: Int, tagBits: Int)extends Module{
   io.hit_st1 := iTagChecker.io.cache_hit
 
   // ******      Replacement    ******
-  val Replacement = Module(new ReplacementUnit(way))
-  Replacement.io.validbits_of_set := Cat(way_valid(io.allocateWrite.bits.setIdx))
+  val Replacement = Module(new ReplacementUnit(Length_Replace_time_SRAM,way))
+  Replacement.io.validOfSet := Cat(way_valid(io.allocateWrite.bits.setIdx))
   io.waymaskReplacement := Replacement.io.waymask
   tagBodyAccess.io.w.req.valid := io.allocateWrite.valid
   io.allocateWrite.ready := tagBodyAccess.io.w.req.ready
@@ -80,20 +95,20 @@ class L1TagAccess(set: Int, way: Int, tagBits: Int)extends Module{
 
 }
 
-class ReplacementUnit(way: Int) extends Module{
+class ReplacementUnit(timeLength:Int, way: Int) extends Module{
   val io = IO(new Bundle {
-    val validbits_of_set = Input(UInt(way.W))
+    val validOfSet = Input(UInt(way.W))
+    val timeOfSet = Input(Vec(way,UInt(timeLength.W)))
     val waymask = Output(UInt(way.W))//one hot
     val Set_is_full = Output(Bool())
   })
-  val victim_1Hidx = if (way>1) RegInit(1.U(way.W)) else RegInit(0.U(1.W))
-  io.Set_is_full := io.validbits_of_set === Fill(way,1.U)
-  io.waymask := Mux(io.Set_is_full, victim_1Hidx, UIntToOH(VecInit(io.validbits_of_set.asBools).indexWhere(_===false.B)))
+  val victim_1Hidx = if (way>1) Wire(UInt(way.W)) else Wire(UInt(1.W))
+  io.Set_is_full := io.validOfSet === Fill(way,1.U)
+  io.waymask := Mux(io.Set_is_full, victim_1Hidx, UIntToOH(VecInit(io.validOfSet.asBools).indexWhere(_===false.B)))
   // First case, set not full
   //Second case, full set, replacement happens
-  if (way>1) victim_1Hidx := RegEnable(Cat(victim_1Hidx(way-2,0),victim_1Hidx(way-1)),io.Set_is_full)
+  if (way>1) victim_1Hidx := 0.U
 }
-
 class tagChecker(way: Int, tagIdxBits: Int) extends Module{
   val io = IO(new Bundle {
     val tag_of_set = Input(Vec(way,UInt(tagIdxBits.W)))//MSB the valid bit
@@ -109,4 +124,26 @@ class tagChecker(way: Int, tagIdxBits: Int) extends Module{
   //io.waymask := Reverse(Cat(io.tag_of_set.map{ tag => (tag(tagIdxBits-1,0) === io.tag_from_pipe) && tag(tagIdxBits)}))
   assert(PopCount(io.waymask) <= 1.U)//if waymask not one-hot, duplicate tags in one set, error
   io.cache_hit := io.waymask.orR
+}
+
+
+class maxIdxTree(width: Int, numInput: Int) extends Module{
+  val treeLevel = log2Ceil(numInput)
+  val io = IO(new Bundle{
+    val candidateIn = Input(Vec(numInput, UInt(width.W)))
+    val idxOfMax = Output(UInt(treeLevel.W))
+  })
+  class candWithIdx extends Bundle{
+    val candidate = UInt(width.W)
+    var index = UInt(treeLevel.W)
+  }
+  def maxWithIdx(a:candWithIdx, b:candWithIdx): candWithIdx = Mux(a.candidate > b.candidate,a,b)
+
+  val candVec = Wire(Vec(numInput,new candWithIdx))
+  for(i <- 0 to numInput-1){
+    candVec(i).candidate := io.candidateIn(i)
+    candVec(i).index := i.asUInt
+  }
+
+  io.idxOfMax := candVec.reduceTree(maxWithIdx(_,_)).index
 }
