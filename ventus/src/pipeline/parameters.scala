a02bbd59ca84958bbef9160d3536cd2ade649c85
yff18 <yufangfei1007@163.com>
2023-03-22 12:02:38 +0800

    cluster2l2 arbiter connection fixed

--- ventus/src/pipeline/parameters.scala
+++ ventus/src/pipeline/parameters.scala
@@ -14,7 +14,7 @@ import L2cache.{CacheParameters, InclusiveCacheMicroParameters, InclusiveCachePa
 import chisel3._
 import chisel3.util._
 object  parameters{//notice log2Ceil(4) returns 2.that is ,n is the total num, not the last idx.
-  def num_sm=8
+  def num_sm=2
 
   val SINGLE_INST:Boolean=false
   val SPIKE_OUTPUT:Boolean=true
@@ -24,7 +24,7 @@ object  parameters{//notice log2Ceil(4) returns 2.that is ,n is the total num, n
   def depth_regBank=log2Ceil(num_warp*32/num_bank)
   def num_warp=8
 
-  def num_cluster = 2
+  def num_cluster = 1
 
   def num_sm_in_cluster = num_sm / num_cluster
   def depth_warp=log2Ceil(num_warp)
