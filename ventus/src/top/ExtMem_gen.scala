199f085c41d40323fbb8d3e2c650346ee455238b
yff18 <yufangfei1007@163.com>
2023-03-22 11:20:40 +0800

    2level arbiter for single L2cache

--- ventus/src/top/ExtMem_gen.scala
+++ ventus/src/top/ExtMem_gen.scala
@@ -11,7 +11,7 @@
 package top
 import L1Cache.MyConfig
 import L2cache.{CacheParameters, InclusiveCacheMicroParameters, InclusiveCacheParameters_lite}
-import pipeline.parameters.{l2cache_BlockWords, l2cache_NSets, l2cache_NWays, l2cache_memCycles, l2cache_portFactor, l2cache_writeBytes, num_sm, num_warp}
+import pipeline.parameters.{l2cache_BlockWords, l2cache_NSets, l2cache_NWays, l2cache_memCycles, l2cache_portFactor, l2cache_writeBytes, num_sm, num_warp,num_cluster,num_sm_in_cluster}
 import pipeline.parameters.{l2cache_BlockWords, l2cache_NSets, l2cache_NWays, l2cache_memCycles, l2cache_portFactor, l2cache_writeBytes, num_sm, num_warp}
 //import pipeline.vALUv2TestWrapper
 
@@ -20,7 +20,7 @@ import pipeline.parameters.{l2cache_BlockWords, l2cache_NSets, l2cache_NWays, l2
 //}
 object GPGPU_gen extends App{
   val L1param = (new MyConfig).toInstance
-  val L2param = InclusiveCacheParameters_lite(CacheParameters(2, l2cache_NSets, l2cache_NWays, blockBytes = (l2cache_BlockWords << 2), beatBytes = (l2cache_BlockWords << 2)), InclusiveCacheMicroParameters(l2cache_writeBytes, l2cache_memCycles, l2cache_portFactor, num_warp, num_sm), false)
+  val L2param = InclusiveCacheParameters_lite(CacheParameters(2, l2cache_NSets, l2cache_NWays, blockBytes = (l2cache_BlockWords << 2), beatBytes = (l2cache_BlockWords << 2)), InclusiveCacheMicroParameters(l2cache_writeBytes, l2cache_memCycles, l2cache_portFactor, num_warp, num_sm, num_sm_in_cluster, num_cluster), false)
   (new chisel3.stage.ChiselStage).emitVerilog(new GPGPU_top()(L1param),Array("--emission-options=disableMemRandomization,disableRegisterRandomization"))
 }
 
