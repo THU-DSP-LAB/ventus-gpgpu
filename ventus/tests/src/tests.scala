6f16d98c512c04149ecc9fad39925584ccf6dc97
au_laptop_wsl <auchufeng@qq.com>
2023-07-21 17:53:54 +0800

    add WWmiss_same.txt

--- ventus/tests/src/tests.scala
+++ ventus/tests/src/tests.scala
@@ -76,7 +76,7 @@ class test_tb extends AnyFreeSpec with ChiselScalatestTester{
   }
 }
 
-class DCache_WWmiss_diff extends AnyFreeSpec with ChiselScalatestTester{
+class DCache_WWmiss_same extends AnyFreeSpec with ChiselScalatestTester{
   val className: String = this.getClass.getSimpleName
   className in {
     test(new DCacheWraper).withAnnotations(Seq(WriteVcdAnnotation)){ DUT =>
