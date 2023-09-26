3d90288a73f24cc09918a7a8b98f293f85a74b05
yff-sh <yufangfei1007@163.com>
2023-09-26 14:16:24 +0800

    mshr subentry valid changed

--- ventus/tests/src/tests.scala
+++ ventus/tests/src/tests.scala
@@ -119,7 +119,7 @@ class AdvancedTest extends AnyFreeSpec with ChiselScalatestTester{ // Working in
   import top.helper._
   "adv_test" in {
     // TODO: rename
-    val testbench = AdvancedTestList.vecadd
+    val testbench = AdvancedTestList.matadd
     val metaFileDir = testbench.meta.map("./ventus/txt/" + testbench.name + "/" + _)
     val dataFileDir = testbench.data.map("./ventus/txt/" + testbench.name + "/" + _)
     val maxCycle = testbench.cycles
