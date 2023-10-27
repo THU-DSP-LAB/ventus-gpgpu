c78acf628aa3b442058cacb16eaa9f5e6aac8753
Vtecbest <lijingzh20@mails.tsinghua.edu.cn>
2023-10-27 14:05:04 +0800

    axi adapter ar valid dependency fixed

--- ventus/src/axi/AXI4Adapter.scala
+++ ventus/src/axi/AXI4Adapter.scala
@@ -64,7 +64,7 @@ class AXI4Adapter (params:  InclusiveCacheParameters_lite_withAXI) extends Modul
 
   io.AXI_master_bundle.aw.valid := (io.l2cache_outa.bits.opcode === PutFullData) && io.l2cache_outa.valid
 
-  io.AXI_master_bundle.ar.valid:=(io.l2cache_outa.bits.opcode === Get) && io.l2cache_outa.fire
+  io.AXI_master_bundle.ar.valid:=(io.l2cache_outa.bits.opcode === Get) && io.l2cache_outa.valid
 
   io.AXI_master_bundle.ar.bits.addr := io.l2cache_outa.bits.address
   io.AXI_master_bundle.ar.bits.size:= (log2Up( params.AXI_params.dataBits/8)).asUInt()
