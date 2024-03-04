0e574e134232d59587d53e06c6803a8aa9a2edd0
Vtecbest <lijingzh20@mails.tsinghua.edu.cn>
2024-03-04 21:18:23 +0800

    add timely hit instead of setquash1

--- ventus/src/L2cache/Directory_test.scala
+++ ventus/src/L2cache/Directory_test.scala
@@ -279,11 +279,12 @@ for(i<- 0 until params.cache.sets){
   }
   val about_replace = (io.write.bits.set === io.result.bits.set) && (io.write.bits.way === io.result.bits.way) && io.write.fire
 
+  val timely_hit = (RegNext(io.read.bits.tag) ===io.write.bits.data.tag) && io.write.fire && (RegNext(io.read.bits.set)===io.write.bits.set)
 
   io.read.ready := ((wipeDone && !io.write.fire()) || (setQuash_1 && tagMatch_1)) && !flush_issue_reg //also fire when bypass
   io.result.valid := Mux(RegNext(flush_issue), RegNext(status_reg(flush_set).dirty(flush_way) && flush_issue), valid_signal)
-  io.result.bits.hit := Mux(RegNext(flush_issue), true.B, (hit || (setQuash && tagMatch )||(setQuash_1 &&tagMatch_1)) && (!about_replace))
-  io.result.bits.way  := Mux(RegNext(flush_issue), RegNext(flush_way),Mux(hit, OHToUInt(hits), Mux(setQuash && tagMatch,RegNext(io.write.bits.way),Mux(setQuash_1&&tagMatch_1,io.write.bits.way,victimWay))))
+  io.result.bits.hit := Mux(RegNext(flush_issue), true.B, (hit || (setQuash && tagMatch )|| timely_hit) && (!about_replace))
+  io.result.bits.way  := Mux(RegNext(flush_issue), RegNext(flush_way),Mux(hit, OHToUInt(hits), Mux(setQuash && tagMatch,RegNext(io.write.bits.way),Mux(timely_hit,io.write.bits.way,victimWay))))
   io.result.bits.put    :=Mux(RegNext(flush_issue),0.U,read_bits_reg.put)
   io.result.bits.data   :=Mux(RegNext(flush_issue),0.U,read_bits_reg.data)
   io.result.bits.offset :=Mux(RegNext(flush_issue),0.U,read_bits_reg.offset)
