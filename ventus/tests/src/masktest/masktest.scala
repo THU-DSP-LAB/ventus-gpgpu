97724b5af497ddfee8caf708796ebd40128b7f7a
xxrrnn <xrn2019@126.com>
2024-04-03 13:02:38 +0800

    sth wrong with the tag

--- ventus/tests/src/masktest/masktest.scala
+++ ventus/tests/src/masktest/masktest.scala
@@ -1,7 +1,7 @@
 import chisel3._
 import chisel3.util._
 import chiseltest._
-//import testparameters._
+import testparameters._
 import org.scalatest.flatspec.AnyFlatSpec
 import scala.util.Random
 
