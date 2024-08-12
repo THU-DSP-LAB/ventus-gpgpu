12602ea1f1a662f226da68e6b0323ff135f24994
yff18 <yufangfei1007@163.com>
2024-08-12 17:46:22 +0800

    MSHR missrspin ready

--- ventus/src/L1Cache/L1MSHR.scala
+++ ventus/src/L1Cache/L1MSHR.scala
@@ -272,7 +272,7 @@ class MSHR(val bABits: Int, val tIWidth: Int, val WIdBits: Int, val NMshrEntry:I
   //如果后面发现missRspOut端口这一级不能取消，使用这段注释掉的代码
   //io.missRspIn.ready := !(subentryStatusForRsp.io.used >= 2.U ||
   //  (subentryStatusForRsp.io.used === 1.U && !io.missRspOut.ready))
-  io.missRspIn.ready := !((subentryStatusForRsp.io.used >= 2.U) ||
+  io.missRspIn.ready := !((subentryStatusForRsp.io.used >= 1.U) ||
     ((mshrStatus_st1_w === 4.U || mshrStatus_st1_w === 3.U) && subentryStatusForRsp.io.used === 1.U) ||
     io.missReq.valid)
 
